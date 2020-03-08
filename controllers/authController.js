0251aa2521f3cb0cd9dc8375f1af2bf1ffd8fe49
Tim M <tim917@gmail.com>
2020-03-08 12:01:41 +0200

    Send emails with nodemailer. HTML to text. Inline email styles

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const crypto = require('crypto');
 const promisify = require('es6-promisify');
 
+const mail = require('../handlers/mail');
+
 const User = mongoose.model('User');
 
 exports.login = passport.authenticate(
@@ -44,8 +46,16 @@ exports.forgot = async (req, res) => {
   await user.save();
 
   // 3. Send an email with the token
-  const resetUrl = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
-  req.flash('success', `You have been emailed a password reset link ${resetUrl}`);
+  const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+
+  await mail.send({
+    user,
+    subject: 'Password Reset',
+    resetURL,
+    filename: 'password-reset',
+  });
+
+  req.flash('success', `You have been emailed a password reset link.`);
 
   // 4. Redirect to login page
   res.redirect('/login');
