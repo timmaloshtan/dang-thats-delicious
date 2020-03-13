2df4befd544f663f3284bd03200799f030c1e672
Tim M <tim917@gmail.com>
2020-03-13 12:16:13 +0200

    Heart updates and animations

--- public/javascripts/modules/heart.js
+++ public/javascripts/modules/heart.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
 
+import { $ } from './bling';
+
 function ajaxHeart(event) {
   event.preventDefault();
-
-  console.log('Heart it!!!!!!!!');
 
   axios.post(this.action)
     .then(({ data }) => {
       const isHearted = this.heart.classList.toggle('heart__button--hearted');
+      $('.heart-count').textContent = data.hearts.length;
+      if (isHearted) {
+        this.heart.classList.add('heart__button--float');
+        setTimeout(() => this.heart.classList.remove('heart__button--float'))
+      }
     });
 };
 
